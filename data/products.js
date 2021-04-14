7d77d9f2bf29c9604018588a66985bc57c38739c
Eniko Kosztolanyi <32710850+3n1k0@users.noreply.github.com>
2021-04-14 08:32:59 +0200

    initial commit

---
+++ data/products.js
@@ -0,0 +1,109 @@
+const products = [
+  {
+    name: "Chloé Woody espadrille with leather details",
+    imageUrl:
+      "https://cdn-1.debijenkorf.nl/web_detail_2x/chloe-woody-espadrille-met-leren-details/?reference=094/380/13_0943805001911400_pro_flt_det_02_1108_1528_5130381.jpg",
+    description:
+      "As an alternative to haute couture creations, Gaby Aghion brought about a fashion revolution in 1952, Chloé's agile aesthetic diverged from women's fashion in the 1950s. Aghion's vision: to give women the freedom to be themselves. Creative Director Natacha Ramsay-Levi wants to create an attitude without imposing a 'look'. The collections are known for their bohemian look and Parisian urbanity.",
+    price: 395,
+    brand: "Chloé",
+    color: "cream, white",
+    countInStock: 0,
+  },
+  {
+    name: "Sandro Sandaal van leer met kettingdetail",
+    imageUrl:
+      "https://cdn-1.debijenkorf.nl/web_detail_2x/sandro-sandaal-van-leer-met-kettingdetail/?reference=026/810/13_0268106109300000_pro_flt_frt_01_1108_1528_5089885.jpg",
+    description: "Materiaal bovenwerk: leer. Materiaal onderwerk: leer",
+    price: 275,
+    brand: "Sandro",
+    color: "cream, white",
+    countInStock: 10,
+  },
+  {
+    name: "Jimmy Choo Bing 65 muiltje van kalfsleer met lakfinish",
+    imageUrl:
+      "https://cdn-1.debijenkorf.nl/web_detail_2x/jimmy-choo-bing-65-muiltje-van-kalfsleer-met-lakfinish/?reference=005/900/13_0059001082600000_pro_flt_frt_01_1108_1528_5071109.jpg",
+    description:
+      "Het succesvolle label Jimmy Choo is in 1996 ontstaan uit een samenwerking tussen de getalenteerde couture schoenmaker Jimmy Choo en de voormalig Britse Vogue Editor Tamara Mellon. De glamoureuze ontwerpen van Jimmy Choo worden handgemaakt in Italië en zijn te herkennen aan de zeer gedetailleerde afwerking. Tegenwoordig is de schoenenlijn uitgebreid met tassen en andere accessoires.",
+    price: 795,
+    brand: "Jimmy Choo",
+    color: "cream, white",
+    countInStock: 2,
+  },
+  {
+    name: "E8 by Miista Elaina muiltje van raffia met leren details",
+    imageUrl:
+      "https://cdn-1.debijenkorf.nl/web_detail_2x/e8-by-miista-elaina-muiltje-van-raffia-met-leren-details/?reference=094/110/13_0941105000755400_pro_flt_frt_01_1108_1528_5114152.jpg",
+    description:
+      "Highlights: Nieuwe merken online, Materiaal bovenwerk: raffia / leer",
+    price: 145,
+    brand: "Miista Elaina",
+    color: "cream, white",
+    countInStock: 25,
+  },
+  {
+    name: "UGG Teague slipper van leer met metallic finish",
+    imageUrl:
+      "https://cdn-1.debijenkorf.nl/web_detail_2x/ugg-teague-slipper-van-leer-met-metallic-finish/?reference=056/060/13_0560605001902400_pro_flt_frt_01_1108_1528_5124118.jpg",
+    description: "Materiaal bovenwerk: leer, Materiaal onderwerk: rubber",
+    price: 64,
+    brand: "UGG",
+    color: "cream, white",
+    countInStock: 4,
+  },
+  {
+    name: "Kurt Geiger Emmy ballerina van raffia met leren details",
+    imageUrl:
+      "https://cdn-1.debijenkorf.nl/web_detail_2x/kurt-geiger-emmy-ballerina-van-raffia-met-leren-details/?reference=098/840/13_0988405013228400_pro_flt_frt_01_1108_1528_5134097.jpg",
+    description: "Kurt Geiger Emmy ballerina van raffia met leren details",
+    price: 120,
+    brand: "Kurt Geiger",
+    countInStock: 10,
+  },
+  {
+    name: "Jimmy Choo Romy 85 pump met glitter details",
+    imageUrl:
+      "https://cdn-1.debijenkorf.nl/web_detail_2x/jimmy-choo-romy-85-pump-met-glitter-details/?reference=005/900/13_0059001083900000_pro_flt_frt_01_1108_1528_5071117.jpg",
+    description:
+      "Highlights: Nieuwe merken online Materiaal bovenwerk: textiel",
+    price: 120,
+    color: "cream, white",
+    brand: "Jimmy Choo",
+    countInStock: 10,
+  },
+  {
+    name: "Fred de la Bretonière Espadrille van leer met metallic finish",
+    imageUrl:
+      "https://cdn-1.debijenkorf.nl/web_detail_2x/fred-de-la-bretoniere-espadrille-van-leer-met-metallic-finish/?reference=034/300/13_0343005001002400_pro_flt_frt_01_1108_1528_5030700.jpg",
+    description: "Highlights: Nieuwe merken online, Materiaal bovenwerk: leer",
+    price: 99,
+    color: "cream, white",
+    brand: "Fred de la Bretonière",
+    countInStock: 10,
+  },
+  {
+    name: "Toral Giusy sneaker met suède details",
+    imageUrl:
+      "https://cdn-1.debijenkorf.nl/web_detail_2x/toral-giusy-sneaker-met-suede-details/?reference=025/360/13_0253605001022400_pro_flt_frt_01_1108_1528_5047962.jpg",
+    description:
+      "Duurzaam: Het leer van dit product is gelooid in een looierij die gecertificeerd is door de Leather Working Group (LWG). De LWG ziet toe op naleving van milieuwetgeving en stimuleert duurzame ontwikkelingen.",
+    price: 244,
+    color: "cream, white",
+    brand: "Toral Giusy",
+    countInStock: 10,
+  },
+  {
+    name: "Dsquared2 Sneaker met glitter en nubuck details",
+    imageUrl:
+      "https://cdn-1.debijenkorf.nl/web_detail_2x/dsquared2-sneaker-met-glitter-en-nubuck-details/?reference=043/530/13_0435305001102400_pro_flt_frt_01_1108_1528_5022660.jpg",
+    description:
+      "Materiaal bovenwerk: nubuck / imitatieleer, Materiaal onderwerk: rubber",
+    price: 120,
+    color: "cream, white",
+    brand: "Dsquared2",
+    countInStock: 10,
+  },
+];
+
+module.exports = products;
---
+++ models/Product.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: false,
+  },
+});
+
+const Product = mongoose.model("product", productSchema);
+
+module.exports = Product;
