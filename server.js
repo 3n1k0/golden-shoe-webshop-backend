267f8f033011ea9c3275f73138e942e34005c691
Eniko Kosztolanyi <32710850+3n1k0@users.noreply.github.com>
2021-04-14 08:45:53 +0200

    update

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const productRoutes = require("./routes/productRoutes");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 connectDB();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "API running..." });
